3cdc790a1cf816c86a93c20d68d1bbff38895005
MooingMiToo <Ogi12tomo16@outlook.com>
2024-05-01 20:42:59 +0900

    5/1

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -8,10 +8,14 @@
     </head>
     <body>
         <h1>お薬管理</h1>
+        <a href='/posts/create'>新規作成</a>
         <div class='posts'>
             @foreach ($posts as $post)
+            
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                    <h2 class='title'>
+                       <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach

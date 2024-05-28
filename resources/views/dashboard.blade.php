916a502c8123bbf1beaac1b45c45fe307a25eff2
MooingMiToo <Ogi12tomo16@outlook.com>
2024-05-28 20:26:28 +0900

    Procfile for Heroku

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,10 +1,11 @@
 <x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('お薬管理') }}
-        </h2>
-    </x-slot>
-
+    <div class="header-container" style="margin-bottom: 20px;">
+        <x-slot name="header">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('お薬管理') }}
+            </h2>
+        </x-slot>
+    </div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-lg sm:rounded-lg">
